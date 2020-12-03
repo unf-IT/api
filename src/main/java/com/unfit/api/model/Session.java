2f0bd6e209503ad10e6816d44a6e04f019aad354
Julien Dudek <julien.dudek@gmail.com>
2020-12-03 19:48:56 +0100

    pom

--- src/main/java/com/unfit/api/model/Session.java
+++ src/main/java/com/unfit/api/model/Session.java
@@ -1,4 +1,22 @@
 package com.unfit.api.model;
 
+import jdk.jfr.DataAmount;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@DataAmount
 public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idSession;
+
+    private Long idFrequentation;
+    private Long idWaterman;
+    private Long idPrelevement;
+
+
 }
