4ccb9dd2f09afd371513482a2269067872873def
Pierre <pierre.darcas@lacatholille.fr>
2020-12-03 23:27:20 +0100

    Waterman

---
+++ src/main/java/com/unfit/api/controllers/WatermanController.java
@@ -0,0 +1,39 @@
+package com.unfit.api.controllers;
+
+import com.unfit.api.converter.WatermanConverter;
+import com.unfit.api.dto.PrelevementDTO;
+import com.unfit.api.dto.WatermanDTO;
+import com.unfit.api.model.Prelevement;
+import com.unfit.api.model.Waterman;
+import com.unfit.api.repositories.WatermanRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/watermen")
+public class WatermanController {
+
+    private static final Logger log = LoggerFactory.getLogger(FrequentationController.class);
+    private final WatermanRepository watermanRepository;
+    private final WatermanConverter watermanConverter;
+
+    public WatermanController(WatermanRepository watermanRepository, WatermanConverter watermanConverter) {
+        this.watermanRepository = watermanRepository;
+        this.watermanConverter = watermanConverter;
+    }
+
+    @GetMapping
+    public List<WatermanDTO> findAll(){
+        log.debug("IN");
+        return watermanConverter.entityToDTO((List<Waterman>) watermanRepository.findAll());
+    }
+
+    @PostMapping
+    public void createWaterman(@RequestBody WatermanDTO watermanDTO){
+        log.debug("IN");
+        watermanRepository.save(watermanConverter.dtoToEntity(watermanDTO));
+    }
+}
